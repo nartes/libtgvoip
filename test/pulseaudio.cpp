d67ded20c07a8013fd12a81708e33a208e90f04d
Siarhei Siniak <serega.belarus@gmail.com>
2018-03-27 22:01:26 +0300

    Rewrite the build

---
+++ test/pulseaudio.cpp
@@ -0,0 +1,6 @@
+#include <test/tgvoip_test.hpp>
+
+TEST(PulseTest, Crash)
+{
+    EXPECT_TRUE(false);
+}
---
+++ test/tgvoip_test.cpp
@@ -0,0 +1,9 @@
+#include <test/tgvoip_test.hpp>
+
+int main(int argc, char *argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
